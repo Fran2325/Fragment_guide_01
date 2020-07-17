9c17cb0ec887530071bd53272182e639d0dbd054
Cris <cristian.vidal.lopezgmail.com>
2020-07-17 15:07:45 -0400

    // This adds view Binding and the logic on the fragment, add the fragment statically

---
+++ app/src/main/java/com/crisspian/fragment_guide_01/QuestionFragment.java
@@ -0,0 +1,67 @@
+package com.crisspian.fragment_guide_01;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.crisspian.fragment_guide_01.databinding.FragmentQuestionBinding;
+
+public class QuestionFragment extends Fragment {
+
+    private FragmentQuestionBinding binding;
+    public static final int YES = 0;
+    public static final int NO = 1;
+
+    public QuestionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentQuestionBinding.inflate(inflater, container, false);
+        final View view = binding.getRoot();
+
+        binding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = binding.radioGroup.findViewById(checkedId);
+                TextView textView = binding.fragmentHeader;
+                int index = binding.radioGroup.indexOfChild(radioButton);
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
